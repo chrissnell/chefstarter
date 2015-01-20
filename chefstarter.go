29e66381ec1b2dda37fe967282d8c4aa9d4eb55b
Christopher Snell <chris.snell@gmail.com>
2015-01-19 23:43:03 -0800

    Commenting

--- chefstarter.go
+++ chefstarter.go
@@ -23,19 +23,20 @@ var (
 func handler(w http.ResponseWriter, r *http.Request) {
 	var err error
 
-	user, pass, authorized := r.BasicAuth()
+	// Pull creds that were passed through HTTP Basic Authentication
+	user, pass, _ := r.BasicAuth()
 
 	if user != *username || pass != *password {
 		log.Println("chefstarter ERROR: Client", r.RemoteAddr, "failed HTTP basic authentication. Username:", user, "Password:", pass)
 		http.Error(w, "Invalid username/password combination", 403)
-	}
+	} else {
+		// HTTP Basic Auth succeeded, so we continue...
 
-	if authorized {
-		log.Println("Launched chef-client by request of", r.RemoteAddr)
+		log.Println("Launching chef-client by request of", r.RemoteAddr)
 
-		//cmd := exec.Command("sudo", "chef-client")
+		cmd := exec.Command("sudo", "chef-client")
 		// For testing chef-client exit codes...
-		cmd := exec.Command("./error.sh")
+		// cmd := exec.Command("./error.sh")
 
 		// Send chef-client's stdout and stderr to chefstarter's stdout and stderr.
 		// If running from supervisord, for example, this can be collected and logged.
@@ -43,15 +44,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		cmd.Stderr = os.Stderr
 
 		if *synch {
+			// If we're running synchronously, block on execution and wait for chef-client to finish
 			err = cmd.Run()
 		} else {
+			// Otherwise, just execute it and move on
 			err = cmd.Start()
 		}
 		if err != nil {
+			// We were unable to execute chef-client or it executed but returned non-zero exit code
 			friendlyErr := "Error executing chef-client: " + err.Error()
 			log.Println("chefstarter ERROR: chef-client execution returned error", err.Error())
 			http.Error(w, friendlyErr, 500)
 		}
+
+		// chef-client ran properly so return a friendly message to the client
 		fmt.Fprintf(w, "chef-client launched")
 		return
 	}
@@ -74,8 +80,10 @@ func main() {
 		log.Fatalln("Must provide a HTTP Basic Auth password.  Use -h for help.")
 	}
 
+	// Set up our handler at the designated path
 	http.HandleFunc(*path, handler)
 
+	// If -ssl=true flag was passed, we use TLS
 	if *usessl {
 		if len(*keyfile) == 0 || len(*crtfile) == 0 {
 			log.Fatalln("For HTTPS, you must specify key and cert file.  Use -h for help.")
@@ -88,6 +96,7 @@ func main() {
 
 	} else {
 
+		// No TLS so we use regular old HTTP
 		http.ListenAndServe(*listen, nil)
 
 	}
